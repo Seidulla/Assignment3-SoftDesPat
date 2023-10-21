6fa885e4e9385b7e14a3a3929c737f503b504bdd
Seidulla <113326965+Seidulla@users.noreply.github.com>
2023-10-21 23:56:18 +0600

    OldLibrary.java

--- OldLibrary.java
+++ OldLibrary.java
@@ -2,6 +2,5 @@
 public class OldLibrary {
 	public void processImageWithOldLibrary(String imagePath) {
         System.out.println("Processing image with old library: " + imagePath);
-        // Actual image processing logic with the old library
     }
 }
