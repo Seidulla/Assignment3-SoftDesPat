1efe72b0231debfee2256ed105cc93606f703b51
Seidulla <113326965+Seidulla@users.noreply.github.com>
2023-10-21 23:56:50 +0600

    ImageAdapter.java

--- ImageAdapter.java
+++ ImageAdapter.java
@@ -11,10 +11,8 @@
 
 	    @Override
 	    public void process(String imagePath) {
-	        // Use the old library's method to process the image
 	        oldLibrary.processImageWithOldLibrary(imagePath);
 
-	        // Use the new library's method to process the image
 	        newLibrary.processImageWithNewLibrary(imagePath);
 	    }
 	}
